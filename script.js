9cebcab07b00741dc1d5d9442ce794a29e92defd
pederseo-dev <pederseo.dev@gmail.com>
2025-12-16 13:26:40 -0300

    se elimino archivos sin utilidad

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-  message.textContent = "¡Hola! Este JS se ejecuta desde Render 🚀";
-});
